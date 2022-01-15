777152b6a00b1cc11d0db4376636811d4a0a3a1f
Edgar9292 <92225390+Edgar9292@users.noreply.github.com>
2022-01-15 17:00:56 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+nota = float(input ("digite sua nota [0.0 - 10.0]:"))
+if nota < 6.0:
+    print('sua nota é F!')
+elif nota < 7.0:
+    print ('sua nota é D!')
+elif nota < 8.0:
+    print('sua nota é C!')
+elif nota < 9.0:
+    print('sua nota é B!')
+else:
+    print ('nota A!')
